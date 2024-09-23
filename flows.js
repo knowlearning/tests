71526482b4c3d18b05effefefc155073091a93f2
Jason Ford <fordjason@gmail.com>
2024-09-23 12:41:45 -0700

    Update tests for local

--- flows.js
+++ flows.js
@@ -1,9 +1,14 @@
 module.exports = { helloFlow } 
 
 async function helloFlow(page) {
-  console.log('RRRRRRRRRRRRRRRRRUNNING!!!!!!!!!!!!!!!!!!')
-  await page.goto('https://thailand.pilaproject.org/teacher')
+  const localPILA = 'https://f74e9cb3-2b53-4c85-9b0c-f1d61b032b3f.localhost:9898'
+  const localAdmin = 'https://localhost:5111'
+  const target = localPILA
+  await page.goto(target)
   await page.waitForSelector('#login-page')
+
+  console.log('GOTLOGIN PAGE???')
+  console.log('GOTLOGIN PAGE???---', await page.locator('#login-page').count())
 
   if (await page.locator('#login-page').count() === 0) {
     throw new Error('No login page!')
