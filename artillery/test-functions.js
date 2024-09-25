4e811be44fb974432b0b1df1ba45d2925dc6883b
Jason Ford <fordjason@gmail.com>
2024-09-24 17:40:23 -0700

    Set up playwright proxy for app metrics to artillery

--- artillery/test-functions.js
+++ artillery/test-functions.js
@@ -1,5 +1,17 @@
-module.exports = { helloFlow } 
+module.exports = { proxy }
 
-async function helloFlow(page, vuContext, events, test) {
-  await page.goto(`/authenticate`)
+async function proxy(page, vuContext, events, test) {
+  let resolve
+  const done = new Promise(r => resolve = r)
+
+  await page.exposeFunction('__report_metric', (type, name, value) => {
+    events.emit(type, `${vuContext.scenario.name} ${name}`, value)
+  })
+
+  await page.exposeFunction('__report_done', resolve)
+
+  console.log('GOING TO', vuContext.scenario.name)
+  await page.goto(vuContext.scenario.name)
+
+  await done
 }
