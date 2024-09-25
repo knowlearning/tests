07aff6fc0baad88eb18a6ee1b478696796ce07d6
Jason Ford <fordjason@gmail.com>
2024-09-25 15:33:30 -0700

    Add test scenarios

--- site/src/scenarios.js
+++ site/src/scenarios.js
@@ -4,5 +4,20 @@ export async function authenticate() {
   const end = Date.now()
 
   await __report_metric('histogram', 'authentication', end - start)
+  await new Promise(r => setTimeout(r, 10_000))
+  await __report_done()
+}
+
+export async function authenticateAndWait10s() { await authenticateAndWait(10) }
+export async function authenticateAndWait20s() { await authenticateAndWait(20) }
+export async function authenticateAndWait30s() { await authenticateAndWait(30) }
+
+async function authenticateAndWait(seconds) {
+  const start = Date.now()
+  await Agent.environment()
+  const end = Date.now()
+
+  await __report_metric('histogram', 'authentication', end - start)
+  await new Promise(r => setTimeout(r, seconds * 1000))
   await __report_done()
 }
